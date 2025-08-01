edf745066affd4497f5993fa08525f24fa2537e7
mimimilog <steps_ts@yahoo.co.jp>
2025-08-01 21:09:10 +1000

    script.jsを修正

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculateTip(percent) {
     if (!isNaN(amount)) {
         const tip = amount * (percent / 100);
         const total = amount + tip;
-        document.getElementById('result').innerHTML = 'チップ: $' + tip.toFixed(2) + ' (' + percent + '%) \n合計: $' + total.toFixed(2);
+        document.getElementById('result').innerHTML = `チップ: $${tip.toFixed(2)} (${percent}%) 合計: $${total.toFixed(2)}`;
     } else {
         document.getElementById('result').innerHTML = '金額を正しく入力してね';
     }
